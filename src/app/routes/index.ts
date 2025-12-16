5c34b29fa12ca2943979c755be3d913e40cd2bb9
abdullah107189 <abdullah107189@gmail.com>
2025-12-16 14:47:07 +0600

    create routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,1 +1,18 @@
-const 
+import express from "express";
+const router = express.Router();
+const moduleRoutes = [
+  {
+    path: "/",
+    route: router,
+  },
+  // {
+  //     path:'/another-path',
+  //     route: another route
+  // }
+];
+moduleRoutes.forEach((route) => {
+  console.log(route);
+  router.use(route.path, route.route);
+});
+
+export default router;
